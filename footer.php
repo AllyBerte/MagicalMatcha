3805fe98cfe964d109ee5784107d7c3e393a2ef2
Alberte Hedegård Schmidt <alberteschmidt@hotmail.com>
2022-10-28 13:52:21 +0200

    Initial commit

---
+++ footer.php
@@ -0,0 +1,80 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <script src="https://kit.fontawesome.com/38af029517.js" crossorigin="anonymous"></script>
+
+ 
+
+</head>
+
+
+<body>
+
+
+
+
+   
+</body>
+
+<footer>
+
+
+    <div class="footer-container">
+
+
+
+        <div class="footer-section1">
+
+
+            <a href="#"class="footer-text z-depth-0">Shop</a>
+            <a href="#"class="footer-attribute z-depth-0">Powder</a>
+            <a href="#"class="footer-attribute z-depth-0">Drinkware</a>
+
+        </div>
+
+        <div class="footer-section2">
+
+            <a href="#"class="footer-text z-depth-0">More</a>
+            <a href="#"class="footer-attribute z-depth-0">Returns</a>
+            <a href="#"class="footer-attribute z-depth-0">Privacy Policy</a>
+
+        </div>
+
+        <div class="footer-section3">
+
+            <a href="#"class="footer-text z-depth-0">Follow Us</a>
+            <i class="fa-brands fa-instagram fa-4x"></i>
+
+        </div>
+
+
+    <div class="footer-container">
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</footer>
+
+
+
+<?php wp_footer() ?>
+</html>
+
+
---
+++ header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php wp_head() ?>
+    <script src="https://kit.fontawesome.com/38af029517.js" crossorigin="anonymous"></script>
+</head>
+<body>
+
+    <nav class="green z-depth-0">
+         <div class="shipping-container">
+        FREE Shipping over 500 kr. Nationwide
+          </div>
+
+        <div class="container">
+            <a href="#" class="brand-logo brand-text">Magical Matcha</a>
+            <ul id="nav-mobile" class="right hand-on-small-and-down">
+                <li><a href="#"class="btn brand z-depth-0">Shop</a></li>
+                <li><a href="#"class="btn brand z-depth-0">Returns</a></li>
+                <li><a href="#"class="btn brand z-depth-0">Privacy Policy</a></li>
+            </ul>
+            <div class="container-nav2">
+                <i class="fa-solid fa-magnifying-glass fa-2x"></i>
+                <i class="fa-solid fa-cart-shopping fa-2x"></i>
+            </div>
+        </div>
+
+
+    </nav>
+     <img src="http://magical-matcha.local/wp-content/uploads/2022/10/matcha.png">
+
+
+
+
+
+    <?php wp_footer() ?>
+</body>
+</html>
